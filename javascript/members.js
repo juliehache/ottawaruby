faa1531d6c4ff8d82ebe0de86a9d46026b98340d
Edward Ocampo-Gooding <edward@edwardog.net>
2009-03-19 01:18:34 -0400

    Added more members and a placeholder message till a carousel is added

--- javascript/members.js
+++ javascript/members.js
@@ -3,6 +3,18 @@ var members = [
   {"name": "Dwayne Litzenberger",
    "projects": [{"name": "Ruby Pixels",
                  "url": "http://www.dlitz.net/software/ruby-pixels/"}]},
+  {"name": "Tobi Lütke",
+   "projects": [{"name": "Liquid",
+                 "url": "http://github.com/tobi/liquid/tree/master"}]},
+  {"name": "Chris Schmitt",
+   "projects": [{"name": "GiftMyList",
+                 "url": "http://www.giftmylist.com/"}]},
+  {"name": "Michael Richardson",
+   "projects": [{"name": "TransitTime",
+                 "url": "http://bluerose.sandelman.ca/projects/show/transittime"}]},
+  {"name": "John Tajima",
+   "projects": [{"name": "Exhibition",
+                 "url": "http://exhibitionquality.com/"}]},
   {"name": "James MacAulay",
    "projects": [{"name": "Active Shipping",
                  "url": "http://github.com/Shopify/active_shipping"}]},
@@ -10,14 +22,18 @@ var members = [
    "projects": [{"name": "Hobo Contrib",
                  "url": "http://bryanlarsen.github.com/hobo-contrib/"}]},
   {"name": "Ryan Lowe",
-   "projects": [{"name": "Acts As Indestructible",
-                 "url": "http://github.com/ryanlowe/acts_as_indestructible/tree/master"}]}
+   "projects": [{"name": "Lighterest",
+                 "url": "http://lighterest.com"}]}
 ];
 
 $(document).ready(function() {
   
   // Shuffle members
   members.sort(function(a,b){return Math.round(Math.random())*-1})
+  
+  var chooseUpTo = 5
+  
+  chooseUpTo = (members.length >= chooseUpTo ? chooseUpTo : members.length)
   
   for (var i=0; i < 4; i++) {
     var member = members.shift();
@@ -28,7 +44,7 @@ $(document).ready(function() {
     var projectElement = $("<div class='projects'></div>").html(
                            $("<a></a>").attr("href", project.url).text(project.name));
 
-    $("#members").append(
+    $("#members").prepend(
       $("<li></li>").append(memberElement).
                      append(projectElement));
   };
