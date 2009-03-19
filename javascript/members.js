360ff440908dc7108f80321242f8532ee212bda6
Edward Ocampo-Gooding <edward@edwardog.net>
2009-03-18 23:37:19 -0400

    Shipping a first, completely static version

---
+++ javascript/members.js
@@ -0,0 +1,15 @@
+/* A database? A data structure? A JSON ghetto-base. */
+var members = [
+  {"name": "Dwayne Litzenberger",
+   "projects": [{"name": "Ruby Pixels",
+                 "url": "http://www.dlitz.net/software/ruby-pixels/"}]},
+  {"name": "James MacAulay",
+   "projects": [{"name": "Active Shipping",
+                 "url": "http://github.com/Shopify/active_shipping"}]},
+  {"name": "Bryan Larsen",
+   "projects": [{"name": "Hobo Contrib",
+                 "url": "http://bryanlarsen.github.com/hobo-contrib/"}]},
+  {"name": "Ryan Lowe",
+   "projects": [{"name": "Acts As Indestructible",
+                 "url": "http://github.com/ryanlowe/acts_as_indestructible/tree/master"}]},
+]
